35d9a788d45bf9beac1588e58f9070434da39eeb
Stephanie Low <62116187+slow312@users.noreply.github.com>
2022-11-21 12:54:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -202,6 +202,8 @@ def login():
             db.session.commit()
             if session['account_type'] == 'admin':
                 return redirect(url_for('get_gui_patients'))
+            elif session['account_type'] == 'careprovider':
+                return redirect(url_for('get_gui_patients'))
             elif session['account_type'] == 'patient':
                 ## go to /details/{{row.mrn}} 
                 return redirect(url_for('get_patient_details', mrn=session['mrn']))
@@ -217,6 +219,9 @@ def register():
         if request.form['account_type'] == 'admin':
             # redirect to admin registration page
             return redirect(url_for('register_admin'))
+        elif request.form['account_type'] == 'careprovider':
+            # redirect to admin registration page
+            return redirect(url_for('register_careprovider'))
         elif request.form['account_type'] == 'patient':
             # redirect to patient registration page
             return redirect(url_for('register_patient'))
@@ -255,6 +260,36 @@ def register_admin():
         msg = 'Please fill out the form!'
     # Show registration form with message (if any)
     return render_template('register_admin.html', msg=msg)
+
+
+@app.route('/register/careprovider', methods=['GET', 'POST'])
+def register_careprovider():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        account_type = 'careprovider'
+        mrn = None
+        ## check if email already exists
+        account = Users.query.filter_by(email=email).first()
+        if account:
+            msg = 'Account already exists !'   
+        else:
+            datecreated = datetime.datetime.now()
+            lastlogin = datetime.datetime.now()
+            new_user = Users(username, password, email, account_type, mrn, datecreated, lastlogin)
+            db.session.add(new_user)
+            db.session.commit()
+            msg = "You have successfully registered a CARE PROVIDER account!"
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register_careprovider.html', msg=msg)
 
 
 @app.route('/register/patient', methods=['GET', 'POST'])
@@ -378,6 +413,9 @@ def logout():
 def get_gui_patients():
     if 'loggedin' in session and session['account_type'] == 'admin':
         returned_Patients = Patients.query.all() # documentation for .query exists: https://docs.sqlalchemy.org/en/14/orm/query.html
+        return render_template("patient_all.html", patients = returned_Patients)
+    if 'loggedin' in session and session['account_type'] == 'careprovider':
+        returned_Patients = Patients.query.all() 
         return render_template("patient_all.html", patients = returned_Patients)
     else:
         return redirect(url_for('get_patient_details', mrn=session['mrn']))
